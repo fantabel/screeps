6e423d255ed80567f82a65bf5b5afc09bf39ef3d
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2017-02-08 07:21:56 -0500

    Integration with home machine

---
+++ trunk/main.js
@@ -0,0 +1,28 @@
+console.log("Init");
+
+var roleHarvester = require('harvester.js');
+var roleUpgrader = require('upgrader.js');
+var roleBuilder = require('builder.js');
+
+module.exports.loop = function() {
+    
+    for(var i in Memory.creeps) {
+       if(!Game.creeps[i]) {
+            delete Memory.creeps[i];
+        }
+    }
+    
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+    
+}
